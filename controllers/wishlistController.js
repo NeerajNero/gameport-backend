0c00c102ceade58ea83e67793d9bfc56e64a3e09
NeerajNero <neerajnero9066@gmail.com>
2024-11-21 08:04:24 +0530

    feature: added all the essential api's for wishlist management

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -61,9 +61,63 @@ const incFromWishlist = async(req,res) => {
             }
         })
         await findWishlist.save()
-        res.status(200).json({message: "wishlist updated successfully", wishlist : findWishlist})
+        res.status(200).json({message: "quantity increased, wishlist updated successfully", wishlist : findWishlist})
     }catch(error){
         res.status(500).json({message: "error occured while incrementing item", error: error.message})
     }
 }
-module.exports = {addToWishlist,getWishlist,incFromWishlist}
+const decFromWishlist = async(req,res) => {
+    try{
+        const {userId, product} = req.body
+        if (!userId || !product) {
+            return res.status(400).json({ message: "User ID and product is required" });
+        }
+        const findWishlist = await Wishlist.findOne({userId})
+        if(!findWishlist){
+            return res.status(404).json({message: "no wishlist found"})
+        }
+        if(findWishlist.items.length === 0){
+            return res.status(404).json({message: "no items in wishlist"})
+        }
+       const findProduct =  findWishlist.items.find((item) => item.product.toString() === product.toString())
+       if(!findProduct){
+        return res.status(404).json({message: "product not found"})
+       }
+       if(findProduct.quantity > 1){
+        findProduct.quantity--
+        await findWishlist.save()
+       res.status(200).json({message: "quantity reduced, wishlist updated successfully", wishlist: findWishlist})
+       }else{
+        findWishlist.items = findWishlist.items.filter((item) => item.product.toString() !== findProduct.product.toString())
+       await findWishlist.save()
+       res.status(200).json({message: "item removed, wishlist updated successfully", wishlist: findWishlist})
+       }
+    }catch(error){
+        res.status(500).json({mesage: "error occured while dec from wishlist",error: error.message})
+    }
+}
+const deleteFromWishlist = async(req,res) => {
+    try{
+        const {userId, product} = req.body
+        if (!userId || !product) {
+            return res.status(400).json({ message: "User ID and product is required" });
+        }
+        const findWishlist = await Wishlist.findOne({userId})
+        if(!findWishlist){
+            return res.status(404).json({message: "no wishlist found"})
+        }
+        if(findWishlist.items.length === 0){
+            return res.status(404).json({message: "no items in wishlist"})
+        }
+        const findProduct = findWishlist.items.find((item) => item.product.toString() === product.toString())
+        if(!findProduct){
+            return res.status(404).json({message: "product not found"})
+        }
+        findWishlist.items = findWishlist.items.filter((item) => item.product.toString() !== findProduct.product.toString())
+        await findWishlist.save()
+        res.status(200).json({message: "wishlist updated successfully", wishlist: findWishlist})
+    }catch(error){
+        res.status(500).json({message: "error occured while deleting item", error: error.message})
+    }
+}
+module.exports = {addToWishlist,getWishlist,incFromWishlist,decFromWishlist,deleteFromWishlist}
--- routes/wishlistRoutes.js
+++ routes/wishlistRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {addToWishlist, getWishlist, incFromWishlist} = require('../controllers/wishlistController')
+const {addToWishlist, getWishlist, incFromWishlist, decFromWishlist, deleteFromWishlist} = require('../controllers/wishlistController')
 const checkToken = require('../middleware/checkToken')
 
 router.post('/addToWishlist', checkToken, addToWishlist)
 router.get('/getWishlist', checkToken, getWishlist)
 router.put('/incFromWishlist', checkToken, incFromWishlist)
+router.put('/decFromWishlist', checkToken, decFromWishlist)
+router.put('/deleteFromWishlist', checkToken, deleteFromWishlist)
 module.exports = router
