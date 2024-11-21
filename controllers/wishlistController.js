952fd8683299928cefee59ed473843ca41c992db
NeerajNero <neerajnero9066@gmail.com>
2024-11-21 06:32:22 +0530

    chore: added fetching wishlist api

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -25,4 +25,23 @@ const addToWishlist = async(req,res) => {
         res.status(500).json({message: "error occured while adding product to wishlist"})
     }
 }
-module.exports = {addToWishlist}
+const getWishlist = async(req,res) => {
+    try{
+        const {userId} = req.body
+        if (!userId) {
+            return res.status(400).json({ message: "User ID is required" });
+        }
+        const findWishlist = await Wishlist.findOne({userId})
+        if(!findWishlist){
+            return res.status(404).json({message: "no wishlist found"})
+        }
+        if(findWishlist.items.length === 0){
+            return res.status(404).json({message: "no items in wishlist"})
+        }
+        res.status(200).json({message: "items in wishlist fetched successfully", items: findWishlist.items})
+    }catch(error)
+    {
+        res.status(500).json({message: "error occured while fetching wishlist"})
+    }
+}
+module.exports = {addToWishlist,getWishlist}
--- routes/wishlistRoutes.js
+++ routes/wishlistRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const {addToWishlist} = require('../controllers/wishlistController')
+const {addToWishlist, getWishlist} = require('../controllers/wishlistController')
 const checkToken = require('../middleware/checkToken')
 
 router.post('/addToWishlist', checkToken, addToWishlist)
+router.get('/getWishlist', checkToken, getWishlist)
 
 module.exports = router
